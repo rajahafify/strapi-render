5ca5580324c2c362789b6765af8ce2bee2f2743f
hafify <hafify@airasia.com>
2022-07-04 04:03:46 +0800

    Add database js for prod

--- config/database.js
+++ config/database.js
@@ -1,11 +1,36 @@
-const path = require('path');
-
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
+if(process.env.NODE_ENV === 'production') {
+  const parse = require('pg-connection-string').parse;
+  const config = parse(process.env.DATABASE_URL);
+  module.exports = ({ env }) => ({
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      client: 'postgres',
+      connection: {
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        user: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false
+        },
+      },
+      debug: false,
     },
-    useNullAsDefault: true,
-  },
-});
+  });
+
+  
+} else {
+  const path = require('path');
+
+  module.exports = ({ env }) => ({
+    connection: {
+      client: 'sqlite',
+      connection: {
+        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      },
+      useNullAsDefault: true,
+    },
+  });
+}
+
+
